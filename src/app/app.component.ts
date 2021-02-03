07782ba71f9bd03a26c02d6461c3cf984401a324
Marjo Pantsar <s2000589@edu.bc.fi>
2021-02-03 13:32:30 +0200

    Finish section 2: Angular Components (https://www.udemy.com/course/angular-for-beginners-course/learn/lecture/12538338#overview)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {COURSES} from '../db-data';
+import { Course } from './model/course';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,19 @@ import {COURSES} from '../db-data';
 })
 export class AppComponent {
 
+  courses = COURSES;
+  startDate = new Date(2000, 0, 1);
+  title = COURSES[0].description;
+  price = 9.9953572655;
+  rate = 0.67;
+  course = COURSES[0]
+
+  onCourseSelected(course: Course) {
+    console.log(course);
+
+  }
+
 
 
 }
+
